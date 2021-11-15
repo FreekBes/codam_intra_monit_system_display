e43b0480225f014f822dad496c0abe050533514c
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2021-11-15 15:43:17 +0100

    added firefox updates
    Mozilla does not want to host the extension on their add-ons page, since the extension is not made for a big enough audience. Releases for Firefox will be released through GitHub exclusively.

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 if [[ $OSTYPE == 'darwin'* ]]; then
 	zip -r chromium.zip manifest.json LICENSE monit.css monit.js images/logo*.png
+	mv manifest.json manifest-chromium.json
+	mv manifest-ff.json manifest.json
 	zip -r firefox.zip manifest.json LICENSE monit.css monit.js images/logo*.png
+	mv manifest.json manifest-ff.json
+	mv manifest-chromium.json manifest.json
 else
-	tar.exe -a -c -f manifest.json LICENSE monit.css monit.js images/logo*.png
-	tar.exe -a -c -f firefox.zip manifest.json LICENSE monit.css monit.js images/logo*.png
+	7z a chromium.zip manifest.json LICENSE monit.css monit.js images/logo*.png
+	mv manifest.json manifest-chromium.json
+	mv manifest-ff.json manifest.json
+	7z a firefox.zip manifest.json LICENSE monit.css monit.js images/logo*.png
+	mv manifest.json manifest-ff.json
+	mv manifest-chromium.json manifest.json
 fi
